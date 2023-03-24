8355c76d9a29debb5213602a64ae4f9c3827d245
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-03-24 18:03:59 +0530

    g

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -6,9 +6,9 @@ import Link from 'next/link'
 import { MoonLoader } from 'react-spinners'
 import { useQueryClient } from '@tanstack/react-query'
 
-const client = useQueryClient()
 
-const feed = () => {
+const Feed = () => {
+  const client = useQueryClient()
 
   const {data:posts,isLoading,error} = api.example.getPosts.useQuery();
   if(isLoading) return<> <MoonLoader color='purple' className='mx-auto my-10'/> </>
@@ -34,4 +34,4 @@ const feed = () => {
   )
 }
 
-export default feed
+export default Feed
