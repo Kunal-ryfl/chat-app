ca89c0af67391a59dc9aa69270f08248184c930e
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-03-25 13:13:51 +0530

    g

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -20,7 +20,7 @@ export default function SignIn({ providers }: InferGetServerSidePropsType<typeof
                         <h2 className="mb-8 text-2xl text-[hsl(280,100%,70%)] font-bold">Sign in  <br></br> to new world .</h2>
                     </div>
                     <div className="mt-16 grid space-y-4">
-                        <button onClick={() => signIn(provider.id)} className="group h-12 px-6 border-2 border-white/10 rounded-full transition duration-300 
+                        <button onClick={() => void signIn(provider.id)} className="group h-12 px-6 border-2 border-white/10 rounded-full transition duration-300 
  hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
                             <div className="relative flex items-center space-x-4 justify-center">
                                 <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" className="absolute left-0 w-5" alt="google logo"/>
