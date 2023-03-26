437b6d38b7a4094fca9b1306d180368e1193f478
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-03-26 12:36:10 +0530

    g

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -36,7 +36,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
- 
+
   callbacks: {
     session({ session, user }) {
       if (session.user) {
@@ -46,7 +46,7 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
-
+    
   },
   adapter: PrismaAdapter(prisma),
   providers: [
